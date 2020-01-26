5fd85ee737276e07ae65682f5df8c7877ced888e
Samuel Yuan <samuel6161@gmail.com>
2020-01-26 05:45:31 -0600

    donate

--- lib/login.dart
+++ lib/login.dart
@@ -1,8 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:codedecoders/homePage.dart';
+import 'package:codedecoders/register.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:line_awesome_icons/line_awesome_icons.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -10,35 +11,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _loginFormKey = new GlobalKey<FormState>();
   TextEditingController emailInputController;
   TextEditingController pwdInputController;
-
-  initState() {
-    emailInputController = new TextEditingController();
-    pwdInputController = new TextEditingController();
-    super.initState();
-  }
-
-  String emailValidator(String value) {
-    Pattern pattern =
-        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-    RegExp regex = new RegExp(pattern);
-    if (!regex.hasMatch(value)) {
-      return 'Email format is invalid';
-    } else {
-      return null;
-    }
-  }
-
-  String pwdValidator(String value) {
-    if (value.length < 8) {
-      return 'Password must be longer than 8 characters';
-    } else {
-      return null;
-    }
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -50,6 +25,7 @@ class _LoginState extends State<Login> {
                 fit: BoxFit.cover,
                 alignment: Alignment.topCenter)),
         child: Form(
+          key: _loginFormKey,
           child: ListView(
             padding: EdgeInsets.all(10),
             children: <Widget>[
@@ -97,16 +73,16 @@ class _LoginState extends State<Login> {
               SizedBox(
                 height: 20,
               ),
-              Container(
-                width: 330,
-                height: 60,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Color(0xff471a91), Color(0xff3cabff)],
+              FlatButton(
+                child: Container(
+                  width: 330,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Color(0xff471a91), Color(0xff3cabff)],
+                    ),
                   ),
-                ),
-                child: Center(
-                  child: FlatButton(
+                  child: Center(
                     child: Text(
                       "Login",
                       style: TextStyle(
@@ -115,29 +91,29 @@ class _LoginState extends State<Login> {
                           fontSize: 18,
                           fontWeight: FontWeight.bold),
                     ),
-                    onPressed: () {
-                      if (_loginFormKey.currentState.validate()) {
-                        FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
+                  ),
+                ),
+                onPressed: () {
+                  print("Pressed");
+                  if (_loginFormKey.currentState.validate()) {
+                    print("login");
+                    FirebaseAuth.instance
+                        .signInWithEmailAndPassword(
                             email: emailInputController.text,
                             password: pwdInputController.text)
-                            .then((currentUser) => Firestore.instance
+                        .then((currentUser) => Firestore.instance
                             .collection("users")
                             .document(currentUser.user.uid)
                             .get()
                             .then((DocumentSnapshot result) =>
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => HomePage(
-                                      uid: currentUser.user.uid,
-                                    ))))
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage(uid: currentUser.user.uid,)),
+                        ))
                             .catchError((err) => print(err)))
-                            .catchError((err) => print(err));
-                      }
-                    },
-                  ),
-                ),
+                        .catchError((err) => print(err));
+                  }
+                },
               ),
               SizedBox(
                 height: 20,
@@ -161,24 +137,32 @@ class _LoginState extends State<Login> {
                     borderRadius: BorderRadius.circular(10),
                     side: BorderSide(color: Color(0xff00b4ff), width: 3)),
                 onPressed: () {},
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Icon(
-                      LineAwesomeIcons.facebook_official,
-                      color: Color(0xff00b4ff),
-                      size: 40,
-                    ),
-                    Text(
-                      "Connect with Facebook",
-                      style: TextStyle(
-                          color: Color(0xff00b4ff),
-                          fontFamily: "CentraleSansRegular",
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
+                child: FlatButton(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Icon(
+                        LineAwesomeIcons.registered,
+                        color: Color(0xff00b4ff),
+                        size: 40,
+                      ),
+                      Text(
+                        "Register",
+                        style: TextStyle(
+                            color: Color(0xff00b4ff),
+                            fontFamily: "CentraleSansRegular",
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RegisterPage()));
+                  },
                 ),
               ),
               SizedBox(
@@ -214,5 +198,30 @@ class _LoginState extends State<Login> {
         ),
       ),
     );
+  }
+
+  String emailValidator(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value)) {
+      return 'Email format is invalid';
+    } else {
+      return null;
+    }
+  }
+
+  initState() {
+    emailInputController = new TextEditingController();
+    pwdInputController = new TextEditingController();
+    super.initState();
+  }
+
+  String pwdValidator(String value) {
+    if (value.length < 8) {
+      return 'Password must be longer than 8 characters';
+    } else {
+      return null;
+    }
   }
 }
